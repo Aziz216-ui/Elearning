7ac8a4f2b7565e3831a4f34f2ace968cc6ca0583
Aziz Melki <aziz@example.com>
2025-11-13 11:06:07 +0100

    Initial commit

---
+++ src/Controller/Front/HomeStatController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeStatController extends AbstractController
+{
+    #[Route('/home/stat', name: 'app_home_stat')]
+    public function index(): Response
+    {
+        return $this->render('front/home_stat/index.html.twig', [
+            'controller_name' => 'HomeStatController',
+        ]);
+    }
+}
